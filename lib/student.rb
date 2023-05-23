12e9c43335ffbf31f8928fad82fd61ac22b13261
Martin <joewwi03@gmail.com>
2023-05-23 09:42:37 +0300

    Completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize 
+        @knowledge = []
+    end
+
+    def learn quote
+        @knowledge  << quote
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
